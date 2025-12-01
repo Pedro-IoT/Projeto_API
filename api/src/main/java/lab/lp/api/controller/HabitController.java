d479702269c45331245db61ea15c377a88fce42c
Pedro Lucas Maia <pedromaiadepaiva@gmail.com>
2025-12-01 11:00:11 -0300

    Adicionando funcionalidades de deletar e marcar como concluído no controller

--- api/src/main/java/lab/lp/api/controller/HabitController.java
+++ api/src/main/java/lab/lp/api/controller/HabitController.java
@@ -1,31 +1,36 @@
 package lab.lp.api.controller;
 
-import lab.lp.api.model.User;
 import lab.lp.api.model.Habit;
 import lab.lp.api.service.HabitService;
-import lab.lp.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/habits")
+@RequestMapping("users/{userId}/habits")
 @RestController
 public class HabitController {
 
     @Autowired
-    private UserService userService;
-
-    @Autowired
     private HabitService habitService;
 
-    @PostMapping("/{userId}")
+    @PostMapping
     public Habit createHabit (@PathVariable Long userId, @RequestBody Habit habit) {
         return habitService.create(habit, userId);
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping
     public List<Habit> listHabits (@PathVariable Long userId) {
         return habitService.habitsList(userId);
+    }
+
+    @DeleteMapping("/{habitId}")
+    public boolean deleteHabit (@PathVariable Long userId, @PathVariable Long habitId) {
+        return habitService.deleteHabit(userId, habitId);
+    }
+
+    @PatchMapping("/{habitId}")
+    public Habit markAsDone (@PathVariable Long userId, @PathVariable Long habitId) {
+        return habitService.markAsDone(userId, habitId);
     }
 }
