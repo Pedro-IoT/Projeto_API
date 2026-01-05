9bf750410d2a244f98f039e33a83729edfd0b915
Pedro-IoT <pedromaiadepaiva@gmail.com>
2026-01-05 09:17:39 -0300

    fix: corrige cors

--- api/src/main/java/lab/lp/api/infra/security/SecurityConfiguration.java
+++ api/src/main/java/lab/lp/api/infra/security/SecurityConfiguration.java
@@ -65,8 +65,10 @@ public class SecurityConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080")
-                        .allowedOrigins("https://localhost:3000")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://localhost:8080"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
             }
         };
